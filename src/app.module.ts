7cb4303627b1a65e16ae4ba07441c070914b6758
ToonyQ <840721193@qq.com>
2024-03-21 00:36:17 +0800

    feat: 添加数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'test',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], // 用于自动加载实体
+      synchronize: true, // 自动创建表
+      retryDelay: 500, // 重试延迟
+      retryAttempts: 10, // 重试次数
+      autoLoadEntities: true, // 自动加载实体
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column()
   email: string;
+  @Column()
   password: string;
-  age: number;
+  @Column()
+  code: string;
+  @Column({name: 'created_at'})
   createdAt: Date;
+  @Column({name: 'updated_at'})
   updatedAt: Date;
 }
