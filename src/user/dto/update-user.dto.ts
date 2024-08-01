a1f2e6fc4d3f1ab5309e86346baf05995551aba7
ToonyQ <840721193@qq.com>
2024-08-01 20:31:19 +0800

    feat: init

--- src/user/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  // Add the fields you want to update here
-  // For example, to update the name and age fields:
-  // name: string;
-  // age: number;
-}
